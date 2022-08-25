2a93867651d0a7c8761d02e74c064ac437b66dcf
Reece Devenney <reecedevenney@gmail.com>
2022-08-24 20:56:33 -0400

    added fetch calls

---
+++ assests/js/script.js
@@ -0,0 +1,21 @@
+var fetchCall = function (movie) {
+    var url = "https://imdb-api.com/en/API/SearchMovie/k_to4td1kd/" + movie
+
+    fetch(url).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data)
+            fetch(`https://api.watchmode.com/v1/title/${data.results[0].id}/details/?apiKey=OnX6Kqfuj26gStUuLHip5UgQYIBVuO7zFbxPETZB&append_to_response=sources`).then(function(resp) {
+                resp.json().then(function (data) {
+                    console.log(data)
+                    fetch(`https://api.watchmode.com/v1/title/${data.similar_titles[0]}/details/?apiKey=OnX6Kqfuj26gStUuLHip5UgQYIBVuO7zFbxPETZB&append_to_response=sources`).then(function(resp) {
+                        resp.json().then(function(data) {
+                            console.log(data)
+                        })
+                    })
+                        })
+                    })
+                })
+            })
+        }
+
+fetchCall("inception")
