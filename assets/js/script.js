64ac06959a97bc2ac79891e1fb3990692354d089
Reece Devenney <reecedevenney@gmail.com>
2022-08-30 19:20:30 -0400

    fixed folder structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,198 @@
-var fetchCall = function (movie) {
-    var url = "https://imdb-api.com/en/API/SearchMovie/k_to4td1kd/" + movie
+var mainSubmitEl = document.querySelector("#main-button")
+var headerSubmitEl = document.querySelector("#header-button")
+var favBtnEl = document.querySelector("#fav-btn")
+var mainInputEl = document.querySelector("#main-input")
+var headerInputEl = document.querySelector("#header-input")
+var largeContainerEl = document.querySelector("#content-container")
+var cardEl = document.querySelector(".card")
 
-    fetch(url).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data)
-            fetch(`https://api.watchmode.com/v1/title/${data.results[0].id}/details/?apiKey=OnX6Kqfuj26gStUuLHip5UgQYIBVuO7zFbxPETZB&append_to_response=sources`).then(function(resp) {
-                resp.json().then(function (data) {
-                    console.log(data)
-                    fetch(`https://api.watchmode.com/v1/title/${data.similar_titles[0]}/details/?apiKey=OnX6Kqfuj26gStUuLHip5UgQYIBVuO7zFbxPETZB&append_to_response=sources`).then(function(resp) {
-                        resp.json().then(function(data) {
-                            console.log(data)
-                        })
-                    })
-                        })
-                    })
-                })
-            })
+// favoriteMovies to get favorites button to work
+var favoriteMovies = []
+
+
+
+var readMainTitle = function(event) {
+    event.preventDefault()
+    var movie = mainInputEl.value.trim()
+
+    fetchCall(movie)
+}
+
+var readHeaderTitle = function(event) {
+    event.preventDefault()
+    var movie = headerInputEl.value.trim()
+
+    fetchCall(movie)
+}
+
+var createCards = function (data) {
+
+    document.getElementById("header-serach").style.visibility = "visible"
+    document.getElementById("main-search").style.display = "none"
+
+        var containerEl = document.createElement("div");
+        containerEl.classList.add("column", "card", "has-background-danger", "m-5")
+
+        var imgEl = document.createElement("img");
+        imgEl.setAttribute("src", data.poster);
+        imgEl.classList.add("card-image", "mx-auto")
+        containerEl.appendChild(imgEl);
+
+        var nameEl = document.createElement("div");
+        nameEl.innerHTML = data.original_title;
+        nameEl.classList.add("card-content", "has-text-weight-bold", "is-size-4")
+        containerEl.appendChild(nameEl);
+
+        var descEl = document.createElement("div");
+        descEl.innerHTML = "Description: " + '"' + data.plot_overview + '"';
+        descEl.classList.add("card-content")
+        containerEl.appendChild(descEl);
+
+        var sitesEl = document.createElement("ul")
+        var sourcesArr = []
+        for (var k = 0; k < data.sources.length; k++) {
+            if (sourcesArr.includes(data.sources[k].name) === false){
+                var site = document.createElement("li");
+                site.innerHTML = data.sources[k].name;
+                sitesEl.appendChild(site);
+                sourcesArr.push(data.sources[k].name)
+            }
+            
         }
+        sitesEl.classList.add("card-content")
+        containerEl.appendChild(sitesEl);
 
-// fetchCall("inception")
+        var favoriteEl = document.createElement("button")
+        favoriteEl.innerHTML = "Favorite"
+        favoriteEl.classList.add("button", "is-warning")
+        containerEl.appendChild(favoriteEl)
 
-// variables
-var img = "https://via.placeholder.com/150"
-var name = "Blood Diamond"
-var desc = "loremipsumkuydktud"
-var sites = ["Netflix", "Hulu", "HBO"]
-// to be edited
-var createCards = function() {
+        largeContainerEl.appendChild(containerEl);
 
 }
+
+var createFavs = function() {
+    largeContainerEl.innerHTML=""
+    document.getElementById("header-serach").style.visibility = "visible"
+    document.getElementById("main-search").style.display = "none"
+
+    for (var i = 0; i<favoriteMovies.length; i++) {
+        var containerEl = document.createElement("div");
+        containerEl.classList.add("column", "card", "has-background-danger", "m-5")
+
+        var imgEl = document.createElement("img");
+        imgEl.setAttribute("src", favoriteMovies[i].image);
+        imgEl.classList.add("card-image", "mx-auto")
+        containerEl.appendChild(imgEl);
+
+        var nameEl = document.createElement("div");
+        nameEl.innerHTML = favoriteMovies[i].title;
+        nameEl.classList.add("card-content", "has-text-weight-bold", "is-size-4")
+        containerEl.appendChild(nameEl);
+
+        var descEl = document.createElement("div");
+        descEl.innerHTML = favoriteMovies[i].description;
+        descEl.classList.add("card-content")
+        containerEl.appendChild(descEl);
+
+
+        var sitesEl = document.createElement("ul")
+        for (var k = 0; k < favoriteMovies[i].services.length; k++) {
+            var site = document.createElement("li");
+
+            site.innerHTML = favoriteMovies[i].services[k];
+            sitesEl.appendChild(site);
+        }
+        sitesEl.classList.add("card-content")
+        containerEl.appendChild(sitesEl);
+
+        var favoriteEl = document.createElement("button")
+        favoriteEl.innerHTML = "Favorite"
+        favoriteEl.classList.add("button", "is-warning")
+        containerEl.appendChild(favoriteEl)
+
+        largeContainerEl.appendChild(containerEl);
+    }
+}
+
+
+
+var saveFavorites = function (event) {
+    // switches text of the button PUT EVERYTHING UNDER THE IF STATMENT, MAKE SURE EVENT ONLY HAPPENS ON BUTTON PRESS
+    if (event.target.classList.contains("button")) {
+        var image = (event.target.parentElement.children[0].src)
+        var title = (event.target.parentElement.children[1].innerHTML)
+        var description = (event.target.parentElement.children[2].innerHTML)
+        //loops to grab all of the listed service options
+        var services = []
+        var i = 0
+        while (event.target.parentElement.children[3].children[i]) {
+            service = (event.target.parentElement.children[3].children[i].innerHTML)
+            services.push(service)
+            i++
+        }
+
+        var chosenFavorite = favoriteMovies.findIndex(favoriteMovies => {return favoriteMovies.title === title})
+
+        if (chosenFavorite === -1) {
+            event.target.innerHTML = "Favorite"
+            var favoriteMovie = {
+                image: image,
+                title: title,
+                description: description,
+                services: services
+            }
+            favoriteMovies.push(favoriteMovie)
+
+        
+            localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies))
+        } else {
+            event.target.innerHTML = "Unfavorite"
+        }
+    // variables for the content of the selectetd box
+   
+    // temp object to fill and push to the array
+    
+    }
+}
+
+var loadSave = function () {
+    var loadMovies = JSON.parse(localStorage.getItem("favoriteMovies"))
+        if (loadMovies) {
+            favoriteMovies = loadMovies
+
+        }
+}
+
+
+
+var fetchCall = function (movie) {
+    var url = "https://imdb-api.com/en/API/SearchMovie/k_to4td1kd/" + movie
+    largeContainerEl.innerHTML = ""
+    fetch(url).then(function (response) {
+        response.json().then(function (data) {
+
+
+            fetch(`https://api.watchmode.com/v1/title/${data.results[0].id}/details/?apiKey=OnX6Kqfuj26gStUuLHip5UgQYIBVuO7zFbxPETZB&append_to_response=sources`).then(function (resp) {
+                resp.json().then(function (data) {
+
+                    for (var z = 0; z < 5; z++) {
+                        fetch(`https://api.watchmode.com/v1/title/${data.similar_titles[z]}/details/?apiKey=OnX6Kqfuj26gStUuLHip5UgQYIBVuO7zFbxPETZB&append_to_response=sources`).then(function (resp) {
+                            resp.json().then(function (data) {
+
+                                createCards(data)
+                            })
+                        })
+                    }
+                })
+            })
+        })
+    })
+}
+
+headerSubmitEl.addEventListener("click", readHeaderTitle)
+mainSubmitEl.addEventListener("click", readMainTitle)
+largeContainerEl.addEventListener("click", saveFavorites)
+favBtnEl.addEventListener("click", createFavs)
+loadSave()
+
